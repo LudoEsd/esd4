d0f21fd796f7619d290317a913a44e164ec9e409
LudoEsd <sochin.chopin@gmail.com>
2016-03-01 12:39:55 +0100

    Added files via upload

---
+++ quizz.py
@@ -0,0 +1,32 @@
+############################
+#QUIZZ
+############################
+
+
+print("Viens tester tes connaissance baby...")
+
+def quiz():
+    print("Question 1")
+    print("qui a creer linux?")
+    print("a. mark zuckenberg")
+    print("b. bill gates")
+    print("c. linus torvald")
+    answer = raw_input("fait ton choix: ")
+    if answer == "c":
+
+			print("Correct!")
+quiz()
+
+def quizz():
+    print("Question 2")
+    print("qui a creer starwars?")
+    print("a. billou")
+    print("b. bill ")
+    print("c. lucas")
+    answer = raw_input("fait ton choix: ")
+    if answer == "c":
+
+			print("Correct!")
+quizz()
+
+
