1e89b0b6ab965153f54dba8969e9c58f77e6cda5
LudoEsd <sochin.chopin@gmail.com>
2016-03-01 12:40:20 +0100

    Added files via upload

---
+++ conditions.py
@@ -0,0 +1,11 @@
+#script conditions
+age = raw_input('entrez votre age:')
+#conditions
+if age < "20":
+	print "vous etes jeune"
+elif age < "40":
+	print "vous etes presque jeune"
+elif age < "60":
+	print  "vous etes bientot mort"
+else:
+	print "vous etes vieux"	
